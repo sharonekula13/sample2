e5833f64bba7c0cc9a03d5682607bc108415eb5e
sharonekula13 <96223501+sharonekula13@users.noreply.github.com>
2021-12-17 12:15:02 +0530

    Create caculator.py

---
+++ caculator.py
@@ -0,0 +1,16 @@
+class A:
+    def __init__(self,i=0):
+        self.i = i
+
+class B(A):
+    def __init__(self,j=0):
+        self.j = j
+
+
+def main():
+    b = B()
+    print(b.i)
+    print(b.j)
+
+main()
+
